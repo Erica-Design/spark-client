ba422daec96752967d68a4d25d6a8a50a9c605f7
MinJunsu <sunsuking@gmail.com>
2023-09-07 12:08:58 +0900

    회원가입 페이지 작업 완료

---
+++ pages/register.tsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import useMobile from "@hooks/useMobile";
+import getDepartments from "@services/departments/get/getDepartments";
+import MobileRegister from "@components/mobile/pages/register";
+import DesktopRegister from "@components/desktop/pages/register";
+import BaseLayout from "@components/base/layout";
+
+interface JwtToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export default function Login() {
+  const isMobile = useMobile();
+
+  const { data: departments, isLoading } = useQuery({
+    queryFn: getDepartments,
+    queryKey: ["departments"],
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <BaseLayout
+      isMobile={isMobile}
+      isFooter={false}
+      isSideBar={false}
+      isHeader={isMobile}
+    >
+      {isMobile ? (
+        <MobileRegister departments={departments} />
+      ) : (
+        <DesktopRegister departments={departments} />
+      )}
+    </BaseLayout>
+  );
+}
