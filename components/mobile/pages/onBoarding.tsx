b9f32420f37208f4b41d6d7b5a21bbf29bc57b01
MinJunsu <sunsuking@gmail.com>
2023-09-08 00:19:23 +0900

    모바일 스파크 페이지 수정'

--- components/mobile/pages/onBoarding.tsx
+++ components/mobile/pages/onBoarding.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import OnBoarding from '../../../icons/onbording.svg'
-import OnBoardLogo from '../../../icons/onboardLogo.svg'
+import OnBoardLogo from "../../../icons/onboardLogo.svg";
 
 export default function MobileOnBoarding() {
   const [countdown, setCountdown] = useState(5);
@@ -26,10 +25,7 @@ export default function MobileOnBoarding() {
   return (
     <div className="bg-black">
       <div className="flex absolute px-[14px] pt-8 w-full">
-        <Image
-          src={OnBoardLogo}
-          alt="로고"
-        />
+        <Image src={OnBoardLogo} alt="로고" />
         <div className="text-white">
           <h1 className="font-['SUIT'] font-normal text-[0.56rem] mt-1">
             SPARK:
@@ -38,21 +34,23 @@ export default function MobileOnBoarding() {
             <span className="font-normal mr-1">ERICA DESIGN</span> ARCHIVING
           </h1>
         </div>
-        <Link href={displayText && '/'}>
+        <Link href={displayText && "/"}>
           <div className=" w-8 h-8 flex-shrink-0 text-[14px] font-bold border border-white absolute flex right-3.5">
-            <span className="self-center mx-auto font-['SUIT'] text-white ml-auto">{displayText || countdown}</span>
+            <span className="self-center mx-auto font-['SUIT'] text-white ml-auto">
+              {displayText || countdown}
+            </span>
           </div>
         </Link>
       </div>
       <div className="flex flex-col items-center justify-center">
         <Image
           className="w-full h-screen"
-          src={OnBoarding}
+          src={"/onBoarding/mobile.gif"}
           alt="온보딩이미지"
         />
         <div className="absolute bottom-10">
           <Link
-            href="/sparkstory"
+            href="/sparkStory"
             className="text-white font-['SUIT'] font-normal text-[14px] underline"
           >
             스파크스토리 보러가기
