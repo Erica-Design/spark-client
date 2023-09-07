07e521454700e7792a4f0d9384d5df03f62d758b
kimkimjunjun <wnsgnl323@gmail.com>
2023-09-07 23:22:27 +0900

    데스크탑 및 모바일 스파크페이지 및 온보딩 UI 완료

--- components/mobile/pages/onBoarding.tsx
+++ components/mobile/pages/onBoarding.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import OnBoarding from '../../../icons/onbording.svg'
+import OnBoardLogo from '../../../icons/onboardLogo.svg'
 
 export default function MobileOnBoarding() {
   const [countdown, setCountdown] = useState(5);
@@ -23,12 +25,30 @@ export default function MobileOnBoarding() {
 
   return (
     <div className="bg-black">
+      <div className="flex absolute px-[14px] pt-8 w-full">
+        <Image
+          src={OnBoardLogo}
+          alt="로고"
+        />
+        <div className="text-white">
+          <h1 className="font-['SUIT'] font-normal text-[0.56rem] mt-1">
+            SPARK:
+          </h1>
+          <h1 className="font-['SUIT'] font-bold text-[0.56rem] mt-[-0.3rem] flex">
+            <span className="font-normal mr-1">ERICA DESIGN</span> ARCHIVING
+          </h1>
+        </div>
+        <Link href={displayText && '/'}>
+          <div className=" w-8 h-8 flex-shrink-0 text-[14px] font-bold border border-white absolute flex right-3.5">
+            <span className="self-center mx-auto font-['SUIT'] text-white ml-auto">{displayText || countdown}</span>
+          </div>
+        </Link>
+      </div>
       <div className="flex flex-col items-center justify-center">
         <Image
           className="w-full h-screen"
-          src="/onBoarding/mobile.gif"
+          src={OnBoarding}
           alt="온보딩이미지"
-          fill
         />
         <div className="absolute bottom-10">
           <Link
